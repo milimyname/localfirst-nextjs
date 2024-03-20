87aef449c462e916b089a57b2683c12d999c781d
mili <kamil.maksudov2019@gmail.com>
2024-03-20 22:54:03 +0100

    zenstack app nextjs init

---
+++ src/lib/hooks/index.ts
@@ -0,0 +1,7 @@
+export * from './post';
+export * from './account';
+export * from './session';
+export * from './user';
+export * from './verification-token';
+export { getQueryKey } from '@zenstackhq/tanstack-query/runtime-v5';
+export { Provider } from '@zenstackhq/tanstack-query/runtime-v5/react';
